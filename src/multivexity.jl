1ed3c92eccd5c0b30a7054ebb053fdc5ce3713db
menyounglee@gmail.com <menyounglee@gmail.com>
2016-09-28 23:36:41 -0400

    bugfixes so it runs in julia-0.5 including { } syntax and Uint

--- src/multivexity.jl
+++ src/multivexity.jl
@@ -16,19 +16,19 @@ multi(v::ConvexVexity) = MultiConvexVexity()
 # records id to variable mapping and
 # adds conflicts to `conflicts`
 function multicurvature(x::AbstractExpr,
-                        conflicts = Set{@compat Tuple{Uint64, Uint64}}(), 
-                        id2var = Dict{Uint64, Variable}())
+                        conflicts = Set{@compat Tuple{UInt64, UInt64}}(),
+                        id2var = Dict{UInt64, Variable}())
   return curvature(x)
 end
 
-function multicurvature(x::MultiplyAtom, 
-                        conflicts = Set{@compat Tuple{Uint64, Uint64}}(), 
-                        id2var = Dict{Uint64, Variable}())
+function multicurvature(x::MultiplyAtom,
+                        conflicts = Set{@compat Tuple{UInt64, UInt64}}(),
+                        id2var = Dict{UInt64, Variable}())
   c1, c2 = x.children[1], x.children[2]
   m1 = multivexity(c1, conflicts, id2var)
-  m2 = multivexity(c2, conflicts, id2var) 
+  m2 = multivexity(c2, conflicts, id2var)
   if m1 != ConstVexity() && m2 != ConstVexity()
-    # one or the other of c1 and c2 must be constant for 
+    # one or the other of c1 and c2 must be constant for
     # the expression to be DCP
     # so we add the complete bipartite graph on the variables in c1 and c2 to the conflict edges E
     union!(conflicts, outerproduct(variablesin(c1, id2var), variablesin(c2, id2var)))
@@ -40,8 +40,8 @@ multivexity(x::Variable, conflicts, id2var) = vexity(x)
 multivexity(x::Constant, conflicts, id2var) = vexity(x)
 
 function multivexity(x::AbstractExpr,
-                     conflicts = Set{@compat Tuple{Uint64, Uint64}}(), 
-                     id2var = Dict{Uint64, Variable}())
+                     conflicts = Set{@compat Tuple{UInt64, UInt64}}(),
+                     id2var = Dict{UInt64, Variable}())
   monotonicities = monotonicity(x)
   vex = multicurvature(x, conflicts, id2var)
   for i = 1:length(x.children)
@@ -51,26 +51,26 @@ function multivexity(x::AbstractExpr,
 end
 
 function multivexity(x::AbstractExpr)
-  conflicts = Set{@compat Tuple{Uint64, Uint64}}()
-  id2var = Dict{Uint64, Variable}()
+  conflicts = Set{@compat Tuple{UInt64, UInt64}}()
+  id2var = Dict{UInt64, Variable}()
   return multivexity(x, conflicts, id2var), conflicts, id2var
 end
 
 # constraints are multiconvex iff they are convex
 multivexity(x::Constraint) = vexity(x)
 
-# a problem is multiconvex if there exists a partition of the variables 
+# a problem is multiconvex if there exists a partition of the variables
 # in the problem such that
   # 1. the objective is convex in each element of the partition holding the others constant
   # 2. all constraints are convex
   # 3. all variables participating in any constraint are in the same element of the partition
-# this function checks multiconvexity of a problem 
+# this function checks multiconvexity of a problem
 # and returns the partition that certifies multiconvexity
 function multivexity(problem::Problem)
   bad_vex = [ConcaveVexity, NotDcp]
 
-  conflicts = Set{@compat Tuple{Uint64, Uint64}}()
-  id2var = Dict{Uint64, Variable}()
+  conflicts = Set{@compat Tuple{UInt64, UInt64}}()
+  id2var = Dict{UInt64, Variable}()
   obj_vex = multivexity(problem.objective, conflicts, id2var)
   if problem.head == :maximize
     obj_vex = -obj_vex
@@ -82,7 +82,7 @@ function multivexity(problem::Problem)
 
   # the constraints must be convex and must not violate the conflicts graph
   m = length(problem.constraints)
-  idpartition = Array(Set{Uint64}, m)
+  idpartition = Array(Set{UInt64}, m)
   activeparts = fill(true, m)
   for i=1:m
     constraint = problem.constraints[i]
@@ -90,7 +90,7 @@ function multivexity(problem::Problem)
       warn("Expression not DMCP compliant: constraint $i not multiconvex")
     end
     vars = variablesin(constraint, id2var)
-    # if the variables in two constraints intersect, 
+    # if the variables in two constraints intersect,
     # the the union of their variables must be in the same partition
     for j=(1:i-1)[activeparts[1:i-1]] # for every subset known to be in the partition
       if length(intersect(idpartition[j], vars)) > 0
@@ -106,14 +106,14 @@ function multivexity(problem::Problem)
   # check that the variables that must be simultaneously active don't violate the conflicts graph
   for s in idpartition
     if !isstableset(s, conflicts)
-      warn("Expression not DMCP compliant: 
-        variables appearing together in a constraint 
-        must be in different subsets of the partition 
+      warn("Expression not DMCP compliant:
+        variables appearing together in a constraint
+        must be in different subsets of the partition
         to ensure the objective is convex.")
     end
   end
 
-  # ok, now partition the remaining variables 
+  # ok, now partition the remaining variables
   # and add them to the appropriate subsets in idpartition
   stablepartition!(conflicts, idpartition)
 
--- src/variablesin.jl
+++ src/variablesin.jl
@@ -5,24 +5,24 @@ hash(x::AbstractExpr) = x.id_hash
 # TODO memoize variables in expressions
 
 # id2var maps variable ids to variables
-function variablesin(x::Variable, id2var = Dict{Uint64, Variable}())
+function variablesin(x::Variable, id2var = Dict{UInt64, Variable}())
   myid = hash(x)
   id2var[myid] = x
-  return Set{Uint64}({myid})
+  return Set{UInt64}([myid])
 end
-variablesin(x::Constant, id2var = Dict{Uint64, Variable}()) = Set{Uint64}()
-function variablesin(x::AbstractExpr, id2var = Dict{Uint64, Variable}())
-  myvars = Set{Uint64}()
+variablesin(x::Constant, id2var = Dict{UInt64, Variable}()) = Set{UInt64}()
+function variablesin(x::AbstractExpr, id2var = Dict{UInt64, Variable}())
+  myvars = Set{UInt64}()
   for child in x.children
     union!(myvars, variablesin(child, id2var))
   end
   myvars
 end
-function variablesin(x::Constraint, id2var = Dict{Uint64, Variable}())
+function variablesin(x::Constraint, id2var = Dict{UInt64, Variable}())
   return union(variablesin(x.rhs, id2var), variablesin(x.lhs, id2var))
 end
-function variablesin(x, id2var = Dict{Uint64, Variable}())
-  warn("why are you asking me this? i'm just a $(typeof(x))"); Set{Uint64}()
+function variablesin(x, id2var = Dict{UInt64, Variable}())
+  warn("why are you asking me this? i'm just a $(typeof(x))"); Set{UInt64}()
 end
 
 # Minkowski or outer product of sets
@@ -30,13 +30,8 @@ function outerproduct{T}(s::Set{T}, t::Set{T})
   results = Set{@compat Tuple{T,T}}()
   for si in s
     for ti in t
-      union!(results, {(si, ti)})
+      union!(results, [(si, ti)])
     end
   end
   return results
 end
-
-
-
-
-
